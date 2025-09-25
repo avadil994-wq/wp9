400dd1bf7d88632b5224d720d18407979c61a491
avadil994-wq <avadil994@gmail.com>
2025-09-25 14:37:03 +0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("wb-cache").then((cache) => {
+      return cache.addAll(["/", "/index.html", "/manifest.json", "/icons/icon-192.png"]);
+    })
+  );
+});
+ 
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
